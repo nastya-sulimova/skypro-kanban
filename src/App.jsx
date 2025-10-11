161b3349f4a5d9eb513e6cca6373c3a3c3f7e693
nastya-sulimova <nastya.sulimova.psk@gmail.com>
2025-10-11 15:55:19 +0300

    добавила картинки

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,16 @@ function App() {
 
   return (
     <>
-         <div id="root"></div>
+         <div id="root">
+         <a href="https://vitejs.dev" target="_blank">
+          {/* Картинка из public - используем прямой путь */}
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          {/* React лого тоже из public */}
+          <img src="/react.svg" className="logo react" alt="React logo" />
+        </a>
+         </div>
          <script type="module" src="/src/main.jsx"></script>
     </>
   )
