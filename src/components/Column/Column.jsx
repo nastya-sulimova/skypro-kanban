040bfa4b2810b1a4e62d3fdcc7200bb5f3de7421
nastya-sulimova <nastya.sulimova.psk@gmail.com>
2025-10-13 20:17:20 +0300

    исправила проблему с вложенностью элементов (поместила компоненты Card и Column на один уровень с Main и Header)

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,4 +1,4 @@
-import Card from './Card/Card';
+import Card from "../Card/Card"
 
 function Column({ title }) {
   return (
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import Column from "./Column/Column"
+import Column from "../Column/Column"
 
 function Main() {
     return (
