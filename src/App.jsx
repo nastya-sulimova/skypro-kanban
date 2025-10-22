941c0617c6df5ba7ac303d22efbb4f24295700f7
nastya-sulimova <nastya.sulimova.psk@gmail.com>
2025-10-22 21:31:48 +0300

    добавлен глобальный стиль GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,56 @@ import PopNewCard from "./components/popups/PopNewCard/PopNewCard";
 import PopUser from "./components/popups/PopUser/PopUser"
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  main.css
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+
+/* CommonLink */
+a,
+a:visited {
+  text-decoration: none;
+  cursor: pointer;
+}
+
+button,
+._btn {
+  cursor: pointer;
+  outline: none;
+}
+
+ul li {
+  list-style: none;
+}
+
+@keyframes card-animation {
+  0% {
+    height: 0;
+    opacity: 0;
+  }
+  100% {
+    height: auto;
+    opacity: 1;
+  }
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+  font-family: "Roboto", Arial, Helvetica, sans-serif;
+  color: #000000;
+}
+`
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -18,6 +68,7 @@ function App() {
 
   return (
     <>
+    <GlobalStyle/>
       <div className="wrapper">
         {/* <!-- pop-up start--> */}
 
