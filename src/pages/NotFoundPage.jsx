6262ed9d0c57891ddf30ddcc72372f79ce328be6
nastya-sulimova <nastya.sulimova.psk@gmail.com>
2025-10-27 22:41:04 +0300

    стилизована страница 404

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,26 @@
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "rgba(148, 166, 190, 1)",
+        fontFamily: "Roboto",
+        fontSize: "30px",
+        fontWeight: "250",
+        lineHeight: "100%",
+        letterSpacing: "0%",
+        textTransform: "uppercase",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div>404</div>
+      <div>Страница не найдена</div>
+    </div>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
