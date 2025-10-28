7bcd2af36428b4959466c317b9e190002ee0caa7
nastya-sulimova <nastya.sulimova.psk@gmail.com>
2025-10-28 22:21:55 +0300

    стилизована страница выхода из аккаунта (компонент popUser.jsx)

--- src/components/popups/PopUser/PopUser.jsx
+++ src/components/popups/PopUser/PopUser.jsx
@@ -1,24 +1,24 @@
 import { Link } from "react-router-dom";
-import { PopExit, PopExitContainer, PopExitBlock } from "./PopUser.styled";
+import { PopExit, PopExitContainer, PopExitBlock, PopExitTitle, PopExitForm, ExitButtonYes, ExitButtonNo } from "./PopUser.styled";
 
 function PopUser() {
     return (
         <PopExit id="popExit">
           <PopExitContainer>
             <PopExitBlock>
-              <div className="pop-exit__ttl">
+              <PopExitTitle>
                 <h2>Выйти из аккаунта?</h2>
-              </div>
-              <form className="pop-exit__form" id="formExit" action="#">
-                <div className="pop-exit__form-group">
-                  <button className="pop-exit__exit-yes _hover01" id="exitYes">
+              </PopExitTitle>
+              <PopExitForm id="formExit" action="#">
+                <div>
+                  <ExitButtonYes id="exitYes">
                     <Link to="/login">Да, выйти</Link>{" "}
-                  </button>
-                  <button className="pop-exit__exit-no _hover03" id="exitNo">
+                  </ExitButtonYes>
+                  <ExitButtonNo id="exitNo">
                     <Link to="/">Нет, остаться</Link>{" "}
-                  </button>
+                  </ExitButtonNo>
                 </div>
-              </form>
+              </PopExitForm>
             </PopExitBlock>
           </PopExitContainer>
         </PopExit>
--- src/components/popups/PopUser/PopUser.styled.js
+++ src/components/popups/PopUser/PopUser.styled.js
@@ -35,3 +35,87 @@ export const PopExitBlock = styled.div`
   border: 0.7px solid #d4dbe5;
   box-shadow: 0px 4px 67px -12px rgba(0, 0, 0, 0.13);
 `;
+
+export const PopExitTitle = styled.div`
+  & h2 {
+    text-align: center;
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 30px;
+    letter-spacing: -0.4px;
+    margin-bottom: 20px;
+  }
+`;
+
+export const PopExitForm = styled.form`
+  & div {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
+
+export const ExitButtonYes = styled.button`
+  width: 153px;
+  height: 30px;
+  background-color: #565eef;
+  border-radius: 4px;
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 21px;
+  font-weight: 500;
+  letter-spacing: -0.14px;
+  color: #ffffff;
+  margin-right: 10px;
+
+  &:hover {
+    background-color: #33399b;
+  }
+
+  & a {
+    width: 100%;
+    height: 100%;
+    color: #ffffff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const ExitButtonNo = styled.button`
+  width: 153px;
+  height: 30px;
+  background-color: transparent;
+  border-radius: 4px;
+  border: 0.7px solid var(--palette-navy-60, #565eef);
+  outline: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 21px;
+  font-weight: 500;
+  letter-spacing: -0.14px;
+  color: #ffffff;
+
+  &:hover {
+    background-color: #33399b;
+  }
+
+  & a {
+    width: 100%;
+    height: 100%;
+    color: #565eef;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:hover {
+      color: #ffffff;
+    }
+  }
+`;
