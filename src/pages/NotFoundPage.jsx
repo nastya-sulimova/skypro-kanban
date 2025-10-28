1160000752411cd8258493c38030d377bf63b8a0
nastya-sulimova <nastya.sulimova.psk@gmail.com>
2025-10-28 22:28:47 +0300

    стилизована NotFoundPage.jsx

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,25 +1,27 @@
+import styled from "styled-components";
+
+const SNotFoundPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: rgba(148, 166, 190, 1);
+  font-family: Roboto;
+  font-size: 30px;
+  font-weight: 250;
+  line-height: 100%;
+  letter-spacing: 0%;
+  text-transform: uppercase;
+  width: 100vw;
+  height: 100vh;
+`;
+
 const NotFoundPage = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        color: "rgba(148, 166, 190, 1)",
-        fontFamily: "Roboto",
-        fontSize: "30px",
-        fontWeight: "250",
-        lineHeight: "100%",
-        letterSpacing: "0%",
-        textTransform: "uppercase",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
+    <SNotFoundPage>
       <div>404</div>
       <div>Страница не найдена</div>
-    </div>
+    </SNotFoundPage>
   );
 };
 
